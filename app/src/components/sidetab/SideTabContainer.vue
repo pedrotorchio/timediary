c4c9befffef23daf65265dccad00fbdf4cc015be
pedrotorchio <pedrotorchio@MBP-de-Pedro.home>
2018-01-13 16:49:54 -0300

    sidetab container height

--- app/src/components/sidetab/SideTabContainer.vue
+++ app/src/components/sidetab/SideTabContainer.vue
@@ -21,14 +21,11 @@ export default {
             gutter: 5,
             side: 64,
             component: null,
-            shown: false
+            shown: false,
+            docHeight: 800
         }
     },
     methods: {
-        top(offset){
-            let gutter = offset == 0 ? 0 : this.gutter;
-            return gutter + offset * this.side + this.topPosition;
-        },
         showComponent(component){
             this.component = component;
             this.show();            
@@ -37,14 +34,25 @@ export default {
             this.shown = value;
         }
     },
+    computed:{
+        maxHeight(){
+            return this.docHeight - this.topPosition;
+        },
+        componentElement(){
+            return this.$refs['side-tab-component'];
+        }
+    },
     components:{
         SideTab
+    },
+    mounted(){
+        this.docHeight = document.body.clientHeight || document.documentElement.clientHeight || document.documentElement.scrollHeight || 800;
     }
 } 
 </script>
 <template>
     <section>
-        <aside class="side-tab-container" :class="[!shown ? 'shown' : '', `_${position}`]" :style='{top:`${top(0)}px`}'>
+        <aside class="side-tab-container" :class="[!shown ? 'shown' : '', `_${position}`]" :style='{top: `${topPosition}px`}'>
                 <side-tab
                 class='tab'
                 
@@ -57,7 +65,7 @@ export default {
                 :disabled='tab.component == null' 
                 @click='showComponent' />
         </aside>
-        <aside class='side-tab-component' :class="[shown ? 'shown' : '', `_${position}`]" :style='{top:`${top(0)}px`}'>
+        <aside ref='side-tab-component' class='side-tab-component' :class="[shown ? 'shown' : '', `_${position}`]" :style='{top: `${topPosition}px`, maxHeight: `${maxHeight}px`}'>
             <v-layout row class="controllers">
                 <v-btn flat icon @click='show(false)'>
                     <v-icon>close</v-icon>
@@ -113,6 +121,7 @@ export default {
         box-sizing: border-box;
         background-color: white;
         border: 4px solid $color__base;
+        overflow-y: scroll;
 
         &._left{
             border-top-right-radius: 10px;
